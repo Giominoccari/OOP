60badba1903c62bfd876b327ce532d1823a4d999
Giovanni Minoccari <98152727+Giominoccari@users.noreply.github.com>
2022-11-11 15:03:46 +0100

    "oop"
    oop

--- oop-lab04-solutions/44-45-rich-design/build.gradle.kts
+++ oop-lab04-solutions/44-45-rich-design/build.gradle.kts
@@ -1,1 +1,28 @@
-plugins { java }
+ plugins {
+    java
+    
+    application
+    
+    id("com.github.johnrengelman.shadow") version "7.0.0" // Update the version!
+}
+
+application {
+    mainClass.set("it.unibo.design.robot.test.TestComposableRobot")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val junitVersion = "5.9.1"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    
+    testLogging.showStandardStreams = true
+}
